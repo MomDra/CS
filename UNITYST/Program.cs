4dfa67747b60a3627f49fd1df8c4074604772c96
MomDra <yem9087@koreatech.ac.kr>
2021-02-12 12:37:11 +0900

    07ClassAccess

---
+++ UNITYST/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Player
+{
+    public int ATT; // 전체 공개
+    protected int HP; // 자식에게만 공개
+    private int MP; // 클래스 내부에만 공개
+
+    public void Fight()
+    {
+        Console.WriteLine("싸운다.");
+    }
+}
+
+namespace UNITYST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player newPlayer = new Player();
+
+            newPlayer.Fight();
+        }
+    }
+}
