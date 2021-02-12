10a58838d785a2f401720a57952d8d6f2b50dfca
MomDra <yem9087@koreatech.ac.kr>
2021-02-12 13:18:56 +0900

    08FuncEx

--- UNITYST/Program.cs
+++ UNITYST/Program.cs
@@ -2,14 +2,50 @@
 
 class Player
 {
-    public int ATT; // 전체 공개
-    protected int HP; // 자식에게만 공개
-    private int MP; // 클래스 내부에만 공개
+    private int ATT; // 전체 공개
+    private int HP; // 자식에게만 공개
+    private int LV; // 클래스 내부에만 공개
 
     public void Fight()
     {
         Console.WriteLine("싸운다.");
     }
+
+    public void setHp(int _HP)
+    {
+        HP = _HP;
+    }
+
+    public void LVUP()
+    {
+        ATT = 100;
+        HP = 1000;
+        LV++;
+    }
+
+    public int getLV()
+    {
+        return LV;
+    }
+
+    public void Damage1(int _Dmg)
+    {
+        HP -= _Dmg;
+    }
+
+    public void Damage2(int _Dmg, int _SubDmg)
+    {
+        HP -= _Dmg;
+        HP -= _SubDmg;
+    }
+
+    public int DamageToHPReturn(int _Dmg)
+    {
+        HP -= _Dmg;
+
+        return HP;
+    }
+
 }
 
 namespace UNITYST
@@ -20,7 +56,12 @@ namespace UNITYST
         {
             Player newPlayer = new Player();
 
-            newPlayer.Fight();
+            newPlayer.LVUP();
+            newPlayer.Damage1(10);
+            newPlayer.Damage2(10, 20);
+
+            Console.WriteLine(newPlayer.getLV());
+            Console.WriteLine(newPlayer.DamageToHPReturn(20));
         }
     }
 }
