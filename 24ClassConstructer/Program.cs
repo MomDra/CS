fca1a358c025ebcf029eabec46c250ee951cd3be
MomDra <yem9087@koreatech.ac.kr>
2021-02-17 01:29:08 +0900

    24ClassConstructer

---
+++ 24ClassConstructer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _24ClassConstructer
+{
+    class FightUnit
+    {
+        protected int AT;
+
+        public FightUnit()
+        {
+            Console.WriteLine("55");
+        }
+    }
+
+    class Player : FightUnit
+    {
+
+
+        public Player()
+        {
+            AT = 5;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player pl = new Player();
+        }
+    }
+}
--- TextRpg/Program.cs
+++ TextRpg/Program.cs
@@ -17,6 +17,24 @@ namespace TextRpg
             Console.WriteLine("체력: {0}/{1}", HP, MAXHP);
             Console.WriteLine("-----------------");
         }
+
+        public bool Fight(FightUnit other)
+        {
+            other.HP -= AT;
+            
+            if(other.HP <= 0)
+            {
+                other.HP = 0;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int getAT()
+        {
+            return AT;
+        }
     }
     class Player : FightUnit
     {
@@ -24,11 +42,20 @@ namespace TextRpg
         {
             HP = MAXHP;
         }
+
+        public Player()
+        {
+            Name = "플레이어";
+            HP = 100;
+        }
     }
 
     class Monster : FightUnit
     {
-
+        public Monster()
+        {
+            Name = "몬스터";
+        }
     }
     enum STARTSELECT
     {
@@ -88,12 +115,51 @@ namespace TextRpg
         static void Battle(Player player)
         {
             Monster newMonster = new Monster();
+            bool Playerturn = true;
+            bool isPlayerDie = false;
+            bool isMonsterDie = false;
             while (true)
             {
                 Console.Clear();
                 player.StatusRender();
                 newMonster.StatusRender();
-                Console.ReadKey();
+
+                if (isPlayerDie)
+                {
+                    Console.WriteLine("플레이어가 죽었습니다. 마을로 복귀합니다.");
+                    Console.ReadKey();
+                    Town(player);
+                    return;
+                }
+                else if (isMonsterDie)
+                {
+                    Console.WriteLine("몬스터를 잡았습니다.. 마을로 복귀합니다.");
+                    Console.ReadKey();
+                    Town(player);
+                    return;
+                }
+
+
+                if (Playerturn)
+                {
+                    isMonsterDie = player.Fight(newMonster);
+                    Console.Clear();
+                    player.StatusRender();
+                    newMonster.StatusRender();
+                    Console.WriteLine("플레이어가 몬스터를 때려 {0}만큼 대미지를 입혔습니다.", player.getAT());
+                    Console.ReadKey();
+                    Playerturn = false;
+                }
+                else
+                {
+                    isPlayerDie = newMonster.Fight(player);
+                    Console.Clear();
+                    player.StatusRender();
+                    newMonster.StatusRender();
+                    Console.WriteLine("몬스터가 플레이어를 때려 {0}만큼 대미지를 입혔습니다.", newMonster.getAT());
+                    Console.ReadKey();
+                    Playerturn = true;
+                } 
             }
         }
 
