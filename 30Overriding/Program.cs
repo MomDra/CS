8c52f68fc63a2cb5ebe1646c2d6f427587f4ac2c
MomDra <yem9087@koreatech.ac.kr>
2021-02-19 18:37:01 +0900

    30Overriding

---
+++ 30Overriding/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _30Overriding
+{
+    class FightUnit
+    {
+        protected int AT = 10;
+
+        public virtual int getAT()
+        {
+            Console.WriteLine("FightUnit의 함수");
+            return AT;
+        }
+    }
+
+    class Player : FightUnit
+    {
+        int ItemAt = 5;
+
+        public override int getAT()
+        {
+            Console.WriteLine("Player의 함수");
+
+            return AT + ItemAt;
+        }
+    }
+
+    class Monster : FightUnit
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            Monster monster = new Monster();
+
+            FightUnit f1 = player;
+            FightUnit f2 = monster;
+
+            f1.getAT();
+            f2.getAT();
+        }
+    }
+}
